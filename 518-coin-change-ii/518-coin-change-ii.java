93ebd97ea0a75f9dbee995dc12430f20d090cc4d
Pragya Singh <86522835+Pragyasingh02@users.noreply.github.com>
2022-09-25 22:47:10 +0530

    Time: 3 ms (93.22%), Space: 41.9 MB (66.13%) - LeetHub

---
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] ch = new int[amount+1];
+        ch[0] = 1;
+        for(int coin : coins)
+        {
+        for(int i=coin;i<=amount;i++)
+        {
+        ch[i] += ch[i-coin];
+        }
+         }
+       return ch[amount];
+
+    }
+}
